81455c3cbf85c28c13608e5ecd6b2144c2b7bad8
Manish Patel <2k24.csai1b.2413477@gmail.com>
2026-07-09 10:35:31 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (5.91%) - LeetSync

---
+++ 1362-airplane-seat-assignment-probability/airplane-seat-assignment-probability.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    double nthPersonGetsNthSeat(int n) {
+        double ans=n;
+        if(n==1)
+            return ans;
+        else 
+            return 0.50000;
+    }
+};
