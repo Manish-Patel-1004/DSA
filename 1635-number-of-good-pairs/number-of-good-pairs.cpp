5e8afc3e7468e8bd21ea735c67ffd9ae33834184
Manish Patel <2k24.csai1b.2413477@gmail.com>
2026-07-22 14:14:35 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (51.74%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.cpp
+++ 1635-number-of-good-pairs/number-of-good-pairs.cpp
@@ -4,7 +4,7 @@ public:
         int n=nums.size();
         int count=0;
         for(int i=0;i<n;i++)
-            for(int j=0;j<n;j++)
+            for(int j=i+1;j<n;j++)
                 if(nums[i]==nums[j]&&i<j)
                     count++;
         return count;
